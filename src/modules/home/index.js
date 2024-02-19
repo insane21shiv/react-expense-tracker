582b8559934801cb254b9a04032c8f673c33c050
insane21siv <shivendras477@gmail.com>
2024-02-19 15:13:09 +0530

    first commit

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -5,12 +5,15 @@ import TransactionComponent from "./TransactionComponent";
 
 
 const Container = styled.div`
+  background-color: white;
+  color: #0d1d2c;
   display: flex;
   flex-direction: column;
+  padding: 10px 22px;
+  font-size: 18px;
+  width: 360px;
   align-items: center;
-  margin: 30px 0 10px;
-  font-family: Montserrat;
-  widht: 360px;
+  justify-content: space-between;
 `;
 
 const HomeComponent = (props) => {
